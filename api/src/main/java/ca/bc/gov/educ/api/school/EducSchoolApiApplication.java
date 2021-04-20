4cb33807c3f4a48b4c958dbb6c041fe1ec39bfc9
s.karekkattumanasree <s.karekkattumanasree@CA-L9Z2KNN2.groupinfra.com>
2021-04-20 11:35:49 -0700

    Using webClient instead of RestTeamplate

--- api/src/main/java/ca/bc/gov/educ/api/school/EducSchoolApiApplication.java
+++ api/src/main/java/ca/bc/gov/educ/api/school/EducSchoolApiApplication.java
@@ -14,6 +14,7 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import ca.bc.gov.educ.api.school.model.dto.School;
 import ca.bc.gov.educ.api.school.model.entity.SchoolEntity;
@@ -43,7 +44,12 @@ public class EducSchoolApiApplication {
 	@Bean
 	public RestTemplate restTemplate(RestTemplateBuilder builder) {
 		return builder.build();
-	}	
+	}
+	
+	@Bean
+	public WebClient webClient() {
+		return WebClient.create();
+	}
 	
 	@Configuration
 	static
