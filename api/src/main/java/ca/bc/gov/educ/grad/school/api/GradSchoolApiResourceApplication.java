b1164c35540fb0e218dde5b12837e6b455c9ec9d
Marco Villeneuve <arcshiftsolutions@gmail.com>
2025-04-15 18:36:04 -0700

    Testing deployment.

--- api/src/main/java/ca/bc/gov/educ/grad/school/api/GradSchoolApiResourceApplication.java
+++ api/src/main/java/ca/bc/gov/educ/grad/school/api/GradSchoolApiResourceApplication.java
@@ -1,5 +1,6 @@
 package ca.bc.gov.educ.grad.school.api;
 
+import lombok.val;
 import net.javacrumbs.shedlock.core.LockProvider;
 import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider;
 import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
@@ -12,10 +13,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -49,13 +51,23 @@ public class GradSchoolApiResourceApplication {
   public LockProvider lockProvider(@Autowired final JdbcTemplate jdbcTemplate,
       @Autowired final PlatformTransactionManager transactionManager) {
     return new JdbcTemplateLockProvider(jdbcTemplate, transactionManager,
-        "INSTITUTE_SHEDLOCK");
+        "GRAD_SCHOOL_SHEDLOCK");
   }
 
+  /**
+   * Thread pool task scheduler thread pool task scheduler.
+   *
+   * @return the thread pool task scheduler
+   */
+  @Bean
+  public ThreadPoolTaskScheduler threadPoolTaskScheduler() {
+    val threadPoolTaskScheduler = new ThreadPoolTaskScheduler();
+    threadPoolTaskScheduler.setPoolSize(5);
+    return threadPoolTaskScheduler;
+  }
 
   /**
-   * The type Web security configuration.
-   * Add security exceptions for swagger UI and prometheus.
+   * The type Web security configuration. Add security exceptions for swagger UI and prometheus.
    */
   @Configuration
   @EnableMethodSecurity
@@ -63,8 +75,8 @@ public class GradSchoolApiResourceApplication {
   class WebSecurityConfiguration {
 
     /**
-     * Instantiates a new Web security configuration.
-     * This makes sure that security context is propagated to async threads as well.
+     * Instantiates a new Web security configuration. This makes sure that security context is
+     * propagated to async threads as well.
      */
     public WebSecurityConfiguration() {
       super();
@@ -73,18 +85,19 @@ public class GradSchoolApiResourceApplication {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
       http
-        .csrf(AbstractHttpConfigurer::disable)
-        .authorizeHttpRequests(auth -> auth
-          .requestMatchers("/v3/api-docs/**",
-            "/actuator/health", "/actuator/prometheus","/actuator/**",
-            "/swagger-ui/**").permitAll()
-          .anyRequest().authenticated()
-        )
-        .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-        .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+              .csrf(AbstractHttpConfigurer::disable)
+              .authorizeHttpRequests(auth -> auth
+                      .requestMatchers("/v3/api-docs/**",
+                              "/actuator/health", "/actuator/prometheus", "/actuator/**",
+                              "/swagger-ui/**").permitAll()
+                      .anyRequest().authenticated()
+              )
+              .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+              .oauth2ResourceServer(oauth2 -> oauth2
+                      .jwt(Customizer.withDefaults())
+              );
       return http.build();
     }
-
   }
 
 }
