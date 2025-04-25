517321ab4f433c89504691fd94c3fa77190c2969
Marco Villeneuve <arcshiftsolutions@gmail.com>
2025-04-25 15:07:58 -0700

    Added scheduler for stan events

--- api/src/main/java/ca/bc/gov/educ/grad/school/api/repository/v1/GradSchoolEventRepository.java
+++ api/src/main/java/ca/bc/gov/educ/grad/school/api/repository/v1/GradSchoolEventRepository.java
@@ -3,12 +3,21 @@ package ca.bc.gov.educ.grad.school.api.repository.v1;
 import ca.bc.gov.educ.grad.school.api.model.v1.GradSchoolEventEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface GradSchoolEventRepository extends JpaRepository<GradSchoolEventEntity, UUID>, JpaSpecificationExecutor<GradSchoolEventEntity> {
     Optional<GradSchoolEventEntity> findByEventId(UUID eventId);
+
+    @Query(value = "select event.* from GRAD_SCHOOL_EVENT event where event.EVENT_STATUS = :eventStatus " +
+            "AND event.CREATE_DATE < :createDate " +
+            "ORDER BY event.CREATE_DATE asc " +
+            "FETCH FIRST :limit ROWS ONLY", nativeQuery=true)
+    List<GradSchoolEventEntity> findAllByEventStatusAndCreateDateBeforeOrderByCreateDate(String eventStatus, LocalDateTime createDate, int limit);
 }
---
+++ api/src/main/java/ca/bc/gov/educ/grad/school/api/schedulers/JetStreamEventScheduler.java
@@ -0,0 +1,56 @@
+package ca.bc.gov.educ.grad.school.api.schedulers;
+
+import ca.bc.gov.educ.grad.school.api.choreographer.ChoreographEventHandler;
+import ca.bc.gov.educ.grad.school.api.repository.v1.GradSchoolEventRepository;
+import lombok.extern.slf4j.Slf4j;
+import net.javacrumbs.shedlock.core.LockAssert;
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+import static ca.bc.gov.educ.grad.school.api.constants.v1.EventStatus.DB_COMMITTED;
+
+
+/**
+ * This class is responsible to check the PEN_MATCH_EVENT table periodically and publish messages to Jet Stream, if some them are not yet published
+ * this is a very edge case scenario which will occur.
+ */
+@Component
+@Slf4j
+public class JetStreamEventScheduler {
+
+  /**
+   * The Event repository.
+   */
+  private final GradSchoolEventRepository eventRepository;
+
+  private final ChoreographEventHandler choreographer;
+
+  /**
+   * Instantiates a new Stan event scheduler.
+   *
+   * @param eventRepository the event repository
+   * @param choreographer   the choreographer
+   */
+  public JetStreamEventScheduler(final GradSchoolEventRepository eventRepository, final ChoreographEventHandler choreographer) {
+    this.eventRepository = eventRepository;
+    this.choreographer = choreographer;
+  }
+
+  /**
+   * Find and process events.
+   */
+  @Scheduled(cron = "${cron.scheduled.process.events.stan}") // every 5 minutes
+  @SchedulerLock(name = "PROCESS_CHOREOGRAPHED_EVENTS_FROM_JET_STREAM", lockAtLeastFor = "${cron.scheduled.process.events.stan.lockAtLeastFor}", lockAtMostFor = "${cron.scheduled.process.events.stan.lockAtMostFor}")
+  public void findAndProcessEvents() {
+    LockAssert.assertLocked();
+    final var results = this.eventRepository.findAllByEventStatusAndCreateDateBeforeOrderByCreateDate(DB_COMMITTED.toString(), LocalDateTime.now().minusMinutes(1), 500);
+    if (!results.isEmpty()) {
+      log.info("found {} choreographed events which needs to be processed.", results.size());
+      results.forEach(this.choreographer::handleEvent);
+    }
+  }
+
+}
